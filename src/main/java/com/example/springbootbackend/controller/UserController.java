dde52b61644b96d5458d9327188da02accd5b294
황인빈 <hibeen1@naver.com>
2022-07-27 11:23:54 +0900

    CORS 수정

--- src/main/java/com/example/springbootbackend/controller/UserController.java
+++ src/main/java/com/example/springbootbackend/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins="*", allowedHeaders = "*")
+@CrossOrigin(origins="http://localhost:3000")
 public class UserController {
 
     private UserService userService;
